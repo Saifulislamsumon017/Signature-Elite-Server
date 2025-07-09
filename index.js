7b632c4bcfafc9cf9f41170d26becf850c0243fd
Saiful Islam <sumon.sis169@gmail.com>
2025-07-09 16:12:26 +0600

    inastall cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000;
@@ -21,7 +22,6 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
     console.log(
